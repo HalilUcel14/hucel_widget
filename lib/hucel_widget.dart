f4c0f18144e1b93e054f7533a08e3f3d000bf7f1
Halil Ucel <hucelsoft@gmail.com>
2022-04-11 09:09:11 +0300

    uniq widget oluşturma

---
+++ lib/hucel_widget.dart
@@ -0,0 +1,10 @@
+library hucel_widget;
+
+// ## - Column Widget - ## //
+export 'src/column_widget/column_widget.dart';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
---
+++ lib/src/row_widget/row_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class PaddingRow extends Padding {
+  PaddingRow({
+    Key? key,
+    required EdgeInsets padding,
+    required List<Widget> children,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    Key? rowKey,
+  }) : super(
+          key: key,
+          padding: padding,
+          child: Row(
+            children: children,
+            crossAxisAlignment: crossAxisAlignment,
+            mainAxisAlignment: mainAxisAlignment,
+            mainAxisSize: mainAxisSize,
+            textBaseline: textBaseline,
+            textDirection: textDirection,
+            verticalDirection: verticalDirection,
+            key: rowKey,
+          ),
+        );
+}
+
+class ExpandedRow extends Expanded {
+  ExpandedRow({
+    Key? key,
+    int flex = 1,
+    required List<Widget> children,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+    Key? rowKey,
+  }) : super(
+          key: key,
+          flex: flex,
+          child: Row(
+            children: children,
+            crossAxisAlignment: crossAxisAlignment,
+            mainAxisAlignment: mainAxisAlignment,
+            mainAxisSize: mainAxisSize,
+            textBaseline: textBaseline,
+            textDirection: textDirection,
+            verticalDirection: verticalDirection,
+            key: rowKey,
+          ),
+        );
+}
+
+class RowInCenter extends Row {
+  RowInCenter({
+    Key? key,
+    required List<Widget> children,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.center,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+  }) : super(
+          key: key,
+          children: children,
+          crossAxisAlignment: crossAxisAlignment,
+          mainAxisAlignment: mainAxisAlignment,
+          mainAxisSize: mainAxisSize,
+          textBaseline: textBaseline,
+          textDirection: textDirection,
+          verticalDirection: verticalDirection,
+        );
+}
