80a66a95b7dc3ed865fc0dd3e18bdd79cb6ade91
HalilUcel <hucelsoft@gmail.com>
2022-04-13 06:38:02 +0300

    version 0.0.2 Yayınlandı

--- lib/hucel_widget.dart
+++ lib/hucel_widget.dart
@@ -4,7 +4,10 @@ library hucel_widget;
 export 'src/column_widget/column_widget.dart';
 // ## - Row Widget - ## //
 export 'src/row_widget/row_widget.dart';
-
+// ## - Container Widget - ## //
+export 'src/container_widget/container_widget.dart';
+// ## - Button Widget - ## //
+export 'src/button_widget/elevated_button.dart';
 // /// A Calculator.
 // class Calculator {
 //   /// Returns [value] plus 1.
